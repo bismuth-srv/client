7057f205f1138a848c83fba55ec6a8278cce505b
Nova Notepad <zeus@zeusteam.dev>
2024-02-18 21:44:42 +1000

    push

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-C:/devkitPro/portlibs/3ds/bin/arm-none-eabi-cmake -S ./source -B ./build
+export DEVKITARM="C:/devkitPro/devkitARM"
+export APP_VERSION="v0.0.1"
+make all
--- source/main.cpp
+++ source/main.cpp
@@ -3,25 +3,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include <3ds.h>
+#include <citro2d.h>
 
 int main(int argc, char* argv[])
 {
 	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+	consoleInit(GFX_BOTTOM, NULL); // Initialize console on the bottom screen
 
 	printf("Hello, world!\n");
 
-	// Main loop
+	// Initialize grid variables
+	const int GRID_SIZE = 5;
+	const int CELL_SIZE = 20;
+	const int GRID_OFFSET_X = 20;
+	const int GRID_OFFSET_Y = 20;
+	bool grid[GRID_SIZE][GRID_SIZE] = { false };
+
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
 		gfxSwapBuffers();
 		hidScanInput();
 
-		// Your code goes here
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
+			break;
+
+		// Clear top screen
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		gspWaitForVBlank();
+		gfxSwapBuffers();
+
+		// Draw grid on top screen
+		for (int i = 0; i < GRID_SIZE; i++)
+		{
+			for (int j = 0; j < GRID_SIZE; j++)
+			{
+				u32 color = grid[i][j] ? RGB565(255, 255, 255) : RGB565(0, 0, 0);
+				C2D_DrawRectSolid(GFX_TOP, GRID_OFFSET_X + i * CELL_SIZE, GRID_OFFSET_Y + j * CELL_SIZE, CELL_SIZE, CELL_SIZE, color);
+			}
+		}
+
+		// Update grid based on user input
+		hidScanInput();
+		u32 kHeld = hidKeysHeld();
+		touchPosition touch;
+		hidTouchRead(&touch);
+		int cellX = (touch.px - GRID_OFFSET_X) / CELL_SIZE;
+		int cellY = (touch.py - GRID_OFFSET_Y) / CELL_SIZE;
+		if (cellX >= 0 && cellX < GRID_SIZE && cellY >= 0 && cellY < GRID_SIZE)
+		{
+			if (kHeld & KEY_TOUCH)
+				grid[cellX][cellY] = true;
+			else if (kHeld & KEY_TOUCH)
+				grid[cellX][cellY] = false;
+		}
 	}
 
 	gfxExit();
